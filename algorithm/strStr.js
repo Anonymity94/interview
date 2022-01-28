08f95b54457141831a466aeab56b0957f703c34d
wangfucheng <wangfucheng@machloop.com>
2022-01-28 17:48:22 +0800

    feat: strStr

---
+++ algorithm/strStr.js
@@ -0,0 +1,45 @@
+// https://leetcode-cn.com/leetbook/read/top-interview-questions-easy/xnr003/
+
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function (haystack, needle) {
+  if (!needle) {
+    return 0;
+  }
+
+  if (needle.length > haystack.length) {
+    return -1;
+  }
+
+  let stackIndex = 0;
+  let needleIndex = 0;
+
+  while (stackIndex < haystack.length) {
+    for (let index = stackIndex; index < haystack.length; index++) {
+      if (haystack[index] === needle[needleIndex]) {
+        // 如果找到了最后一个，直接返回栈的索引位置
+        if (needleIndex === needle.length - 1) {
+          return stackIndex;
+        } else {
+          needleIndex++;
+          continue;
+        }
+      } else {
+        stackIndex++;
+        needleIndex = 0;
+        break;
+      }
+    }
+  }
+
+  return -1;
+};
+
+console.log(strStr("hello", "ll"));
+console.log(strStr("aaaaa", "bba"));
+console.log(strStr("", ""));
+console.log(strStr("aaa", "aaaa"));
+console.log(strStr("mississippi", "issip"));
