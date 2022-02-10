086ba790d92067394ef4c8fcdf608aa5954a9f66
anonymity <1176989591@qq.com>
2022-02-10 23:10:43 +0800

    feat: Promise

--- js/promise.js
+++ js/promise.js
@@ -6,6 +6,8 @@ class MyPromise {
   constructor(func) {
     this.PromiseState = MyPromise.PENDING;
     this.PromiseResult = null;
+    this.onFulfilledCallbacks = []; // 成功的回调
+    this.onRejectedCallbacks = []; // 失败的回调
 
     try {
       func(this.resolve.bind(this), this.reject.bind(this));
@@ -15,14 +17,24 @@ class MyPromise {
   }
   resolve(result) {
     if (this.PromiseState === MyPromise.PENDING) {
-      this.PromiseState = MyPromise.FULFILLED;
-      this.PromiseResult = result;
+      setTimeout(() => {
+        this.PromiseState = MyPromise.FULFILLED;
+        this.PromiseResult = result;
+        this.onFulfilledCallbacks.forEach((callback) => {
+          callback(result);
+        });
+      });
     }
   }
   reject(reason) {
     if (this.PromiseState === MyPromise.PENDING) {
-      this.PromiseState = MyPromise.REJECTED;
-      this.PromiseResult = reason;
+      setTimeout(() => {
+        this.PromiseState = MyPromise.REJECTED;
+        this.PromiseResult = reason;
+        this.onRejectedCallbacks.forEach((callback) => {
+          callback(reason);
+        });
+      });
     }
   }
   then(onFulfilled, onRejected) {
@@ -35,16 +47,135 @@ class MyPromise {
             throw reason;
           };
 
-    if (this.PromiseState === MyPromise.FULFILLED) {
-      setTimeout(() => {
-        onFulfilled(this.PromiseResult);
-      });
+    const promise2 = new MyPromise((resolve, reject) => {
+      if (this.PromiseState === MyPromise.PENDING) {
+        this.onFulfilledCallbacks.push(() => {
+          setTimeout(() => {
+            try {
+              const x = onFulfilled(this.PromiseResult);
+              resolvePromise(promise2, x, resolve, reject);
+            } catch (error) {
+              reject(error);
+            }
+          });
+        });
+        this.onRejectedCallbacks.push(() => {
+          setTimeout(() => {
+            try {
+              const x = onRejected(this.PromiseResult);
+              resolvePromise(promise2, x, resolve, reject);
+            } catch (error) {
+              reject(error);
+            }
+          });
+        });
+      }
+
+      if (this.PromiseState === MyPromise.FULFILLED) {
+        setTimeout(() => {
+          try {
+            const x = onFulfilled(this.PromiseResult);
+            resolvePromise(promise2, x, resolve, reject);
+          } catch (error) {
+            reject(error);
+          }
+        });
+      }
+      if (this.PromiseState === MyPromise.REJECTED) {
+        setTimeout(() => {
+          try {
+            const x = onRejected(this.PromiseResult);
+            resolvePromise(promise2, x, resolve, reject);
+          } catch (error) {
+            reject(error);
+          }
+        });
+      }
+    });
+    return promise2;
+  }
+}
+
+/**
+ * 对resolve()、reject() 进行改造增强 针对resolve()和reject()中不同值情况 进行处理
+ * @param  {promise} promise2 promise1.then方法返回的新的promise对象
+ * @param  {[type]} x         promise1中onFulfilled或onRejected的返回值
+ * @param  {[type]} resolve   promise2的resolve方法
+ * @param  {[type]} reject    promise2的reject方法
+ */
+function resolvePromise(promise2, x, resolve, reject) {
+  // 如果从onFulfilled或onRejected中返回的 x 就是promise2，会导致循环引用报错
+  if (x === promise2) {
+    return reject(new TypeError("Chaining cycle detected for promise"));
+  }
+
+  if (x instanceof MyPromise) {
+    if (x.PromiseState === MyPromise.PENDING) {
+      x.then((y) => {
+        resolvePromise(promise2, y, resolve, reject);
+      }, reject);
     }
-    if (this.PromiseState === MyPromise.REJECTED) {
-      setTimeout(() => {
-        onRejected(this.PromiseResult);
-      });
+  } else if (x.PromiseState === MyPromise.FULFILLED) {
+    // 2.3.2.2 如果 x 处于执行态，用相同的值执行 promise
+    resolve(x.PromiseResult);
+  } else if (x.PromiseState === MyPromise.REJECTED) {
+    // 2.3.2.3 如果 x 处于拒绝态，用相同的据因拒绝 promise
+    reject(x.PromiseResult);
+  } else if (x !== null && (typeof x === "object" || typeof x === "function")) {
+    // 2.3.3 如果 x 为对象或函数
+    try {
+      // 2.3.3.1 把 x.then 赋值给 then
+      var then = x.then;
+    } catch (e) {
+      // 2.3.3.2 如果取 x.then 的值时抛出错误 e ，则以 e 为据因拒绝 promise
+      return reject(e);
     }
+
+    /**
+     * 2.3.3.3
+     * 如果 then 是函数，将 x 作为函数的作用域 this 调用之。
+     * 传递两个回调函数作为参数，
+     * 第一个参数叫做 `resolvePromise` ，第二个参数叫做 `rejectPromise`
+     */
+    if (typeof then === "function") {
+      // 2.3.3.3.3 如果 resolvePromise 和 rejectPromise 均被调用，或者被同一参数调用了多次，则优先采用首次调用并忽略剩下的调用
+      let called = false; // 避免多次调用
+      try {
+        then.call(
+          x,
+          // 2.3.3.3.1 如果 resolvePromise 以值 y 为参数被调用，则运行 [[Resolve]](promise, y)
+          (y) => {
+            if (called) return;
+            called = true;
+            resolvePromise(promise2, y, resolve, reject);
+          },
+          // 2.3.3.3.2 如果 rejectPromise 以据因 r 为参数被调用，则以据因 r 拒绝 promise
+          (r) => {
+            if (called) return;
+            called = true;
+            reject(r);
+          }
+        );
+      } catch (e) {
+        /**
+         * 2.3.3.3.4 如果调用 then 方法抛出了异常 e
+         * 2.3.3.3.4.1 如果 resolvePromise 或 rejectPromise 已经被调用，则忽略之
+         */
+        if (called) return;
+        called = true;
+
+        /**
+         * 2.3.3.3.4.2 否则以 e 为据因拒绝 promise
+         */
+        reject(e);
+      }
+    } else {
+      // 2.3.3.4 如果 then 不是函数，以 x 为参数执行 promise
+      resolve(x);
+    }
+  } else {
+    // 2.3.4 如果 x 不为对象或者函数，以 x 为参数执行 promise
+    return resolve(x);
   }
 }
 
@@ -55,8 +186,6 @@ let promise1 = new MyPromise((resolve, reject) => {
 promise1.then(undefined, (err) => {
   console.log(err);
 });
-
-console.log(promise1);
 
 let promise2 = new MyPromise((resolve, reject) => {
   reject("下次一定");
@@ -78,9 +207,27 @@ console.log(1);
 let promise4 = new MyPromise((resolve, reject) => {
   console.log(2);
   setTimeout(() => {
+    console.log("A", promise4.PromiseState);
     resolve("这次一定");
+    console.log("B", promise4.PromiseState);
     console.log(4);
   });
 });
-promise4.then((result) => console.log(result));
+promise4.then((result) => {
+  console.log("C", promise4.PromiseState);
+  console.log(result);
+});
+promise4.then((result) => {
+  console.log("D", promise4.PromiseState);
+  console.log(result);
+});
+promise4
+  .then((result) => {
+    console.log("E", promise4.PromiseState);
+    console.log(result);
+    return 20;
+  })
+  .then((res) => {
+    console.log("F", res);
+  });
 console.log(3);
