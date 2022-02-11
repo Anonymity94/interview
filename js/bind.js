3aa7d982d6daac83f2bda05ea11efb36e1555e9a
wangfucheng <wangfucheng@machloop.com>
2022-02-11 15:05:22 +0800

    feat: bind

---
+++ js/bind.js
@@ -0,0 +1,44 @@
+// https://github.com/mqyqingfeng/Blog/issues/12
+Function.prototype.bind2 = function (context) {
+  if (typeof this !== "function") {
+    throw new Error(
+      "Function.prototype.bind - what is trying to be bound is not callable"
+    );
+  }
+
+  var self = this;
+  var args = Array.prototype.slice.call(arguments, 1);
+
+  var fNOP = function () {};
+
+  var fBound = function () {
+    var bindArgs = Array.prototype.slice.call(arguments);
+    return self.apply(
+      this instanceof fNOP ? this : context,
+      args.concat(bindArgs)
+    );
+  };
+
+  fNOP.prototype = this.prototype;
+  fBound.prototype = new fNOP();
+  return fBound;
+};
+
+var foo = {
+  value: 1,
+};
+
+function bar(name, age) {
+  console.log(this.value);
+  return {
+    value: this.value,
+    name: name,
+    age: age,
+  };
+}
+
+var fn = bar.bind2(foo, "kevin");
+console.log(fn(18));
+
+var obj = new fn("18");
+console.log(obj);
